f9178579402bdb79212f61bc8314b5537311424b
HusamMohammed <husam.m.ashour@gmail.com>
2024-08-28 01:14:07 +0300

    apply prettier

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background text-body`}>
-        {children}
-      </body>
+      <body className={`${inter.className} bg-background text-body`}>{children}</body>
     </html>
   );
 }
