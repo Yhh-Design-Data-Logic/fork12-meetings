f5b30a116a4c641a9d07c365ece5a1180e08d416
HusamMohammed <husam.m.ashour@gmail.com>
2024-09-01 04:39:02 +0300

    enhance header for large screens

--- src/components/layout/constants.ts
+++ src/components/layout/constants.ts
@@ -7,3 +7,14 @@ export const teacherPages = [
   { href: "/", title: "Meetings", icon: "dashboard" },
   { href: "/calendar", title: "Calendar", icon: "calendar" },
 ] as const;
+
+export const pageNames = [
+  {
+    url: "/",
+    name: "My Meetings",
+  },
+  {
+    url: "/calendar",
+    name: "My Calendar",
+  },
+];
--- src/components/layout/elements.tsx
+++ src/components/layout/elements.tsx
@@ -37,3 +37,38 @@ export const NavItem = ({
     </li>
   );
 };
+
+export const ProfileInfo = ({
+  name,
+  email,
+}: {
+  name: string;
+  email: string;
+}) => (
+  <div className="flex items-center space-x-2 before:mx-3 before:h-10 before:w-px before:bg-zinc-200">
+    <Avatar className="h-11 w-11">
+      <AvatarFallback>H</AvatarFallback>
+    </Avatar>
+
+    <div className="flex flex-col">
+      <span className="text-sm font-bold">{name}</span>
+      <span className="text-xs text-body-light">
+        {email.length > 20 ? email.substring(0, 20) + "..." : email}
+      </span>
+    </div>
+  </div>
+);
+
+export const PageHeading = () => {
+  const pathname = usePathname();
+
+  const pageName = pageNames.find(({ url }) =>
+    // check if it's a nested route
+    pathname.split("/").length > 2 ? pathname.includes(url) : pathname === url
+  )?.name;
+
+  if (!pageName)
+    throw new Error(`Page name is found for this page url: ${pathname}.`);
+
+  return <h1 className="text-3xl font-bold text-heading">{pageName}</h1>;
+};
