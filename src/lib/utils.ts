a0fdd4daeedf89d2b17985b85a583deb6e84c1c2
HusamMohammed <husam.m.ashour@gmail.com>
2024-09-03 18:35:15 +0300

    add directue error predicate

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ZodError } from "zod";
 
+import { DirectusError } from "@/types";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -9,3 +11,11 @@ export function cn(...inputs: ClassValue[]) {
 export function isZodError(err: unknown): err is ZodError {
   return err instanceof ZodError;
 }
+
+export const isDirectusError = (error: unknown): error is DirectusError => {
+  if (error && typeof error === "object" && "errors" in error) {
+    return true;
+  }
+
+  return false;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,8 @@ export enum UserType {
   TEACHER = "Teacher",
   PARENT = "Parent",
 }
+
+export type DirectusError = {
+  errors: { message: string; extensions: { code: string } }[];
+  response?: Response;
+};
