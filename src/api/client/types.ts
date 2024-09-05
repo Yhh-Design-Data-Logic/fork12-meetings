cf30baf5419e757619e54ea26e25b49665be7cb2
HusamMohammed <husam.m.ashour@gmail.com>
2024-09-05 16:30:45 +0300

    add api schema

--- src/api/client/types.ts
+++ src/api/client/types.ts
@@ -1,7 +1,62 @@
+import { DirectusUser } from "@directus/sdk";
+
+export type Meeting = {
+  id: number;
+  parent: number | Parent;
+  teacher: number | Teacher;
+  timeslot: number | Timeslot;
+};
+
+export type Parent = {
+  id: number;
+  name: string;
+  user: string | DirectusUser<CustomDirectusUser>;
+  children: number[];
+  meetings: number[];
+};
+export type Child = {
+  id: number;
+  name: string;
+  parent: number | Parent;
+  teachers: number[] | ChildrenTeachers[];
+};
+
+export type Teacher = {
+  id: number;
+  name: string;
+  user: string | DirectusUser<CustomDirectusUser>;
+  timeslots: number[] | Timeslot[];
+  meetings: number[] | Meeting[];
+};
+
+export type Timeslot = {
+  id: number;
+  teacher: number | Teacher;
+  start_date: string;
+  end_date: string;
+  meetings: number[] | Meeting[];
+};
+
+type ChildrenTeachers = {
+  id: number;
+  children_id: number | Parent;
+  teachers_id: number | Teacher;
+};
+
 interface CustomDirectusUser {
   type: "parent" | "teacher";
+  parent: Parent | null;
+  teacher: Teacher | null;
 }
 
 export interface Schema {
+  meetings: Meeting[];
+  parents: Parent[];
+  children: Child[];
+  teachers: Teacher[];
+  timeslots: Timeslot[];
+
+  children_teachers: ChildrenTeachers[];
+
   directus_users: CustomDirectusUser;
 }
