8c267a7b008d3565da37768454e6c714587d4361
HusamMohammed <husam.m.ashour@gmail.com>
2024-09-06 16:41:37 +0300

    add baseDate option to isSameDay utility

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -4,6 +4,6 @@ export const formatDate = (date: string | Date, format: string) => {
   return dayjs(date).format(format);
 };
 
-export const isSameDay = (date: string | Date) => {
-  return dayjs().isSame(date, "day");
+export const isSameDay = (date: string | Date, baseDate?: string | Date) => {
+  return dayjs(baseDate).isSame(date, "day");
 };
