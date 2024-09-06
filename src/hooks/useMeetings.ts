522dec64f12d9c928479fc161b8cfb1b67bb88c8
HusamMohammed <husam.m.ashour@gmail.com>
2024-09-06 17:23:05 +0300

    filter out meetings with no timeslots

--- src/hooks/useMeetings.ts
+++ src/hooks/useMeetings.ts
@@ -8,15 +8,18 @@ export const useMeetings = () => {
   return useQuery({
     queryKey: ["meetings"],
     queryFn: meetingsApi.getAll,
-    select: (data) =>
-      data.map((meeting) => ({
-        id: meeting.id,
-        name:
-          getUserSessionFromStorage().type === UserType.PARENT
-            ? meeting.teacher.name
-            : meeting.parent.name,
-        startDate: meeting.timeslot.start_date,
-        endDate: meeting.timeslot.end_date,
-      })),
+    select: (data) => {
+      return data
+        .filter((m) => Boolean(m.timeslot))
+        .map((meeting) => ({
+          id: meeting.id,
+          name:
+            getUserSessionFromStorage().type === UserType.PARENT
+              ? meeting.teacher.name
+              : meeting.parent.name,
+          startDate: meeting.timeslot.start_date,
+          endDate: meeting.timeslot.end_date,
+        }));
+    },
   });
 };
