ff86a9103ef902e4a1d1dca8c1f4d08624e08b53
aeymash <abdallatif.ey@gmail.com>
2024-09-24 10:10:15 +0300

    Add Deployment files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import("./src/lib/env-validation.mjs");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+};
 
 export default nextConfig;
