9c931d8e422891f5df1f6f98ac51f00959efd7de
HusamMohammed <husam.m.ashour@gmail.com>
2024-10-08 10:19:04 +0300

    add icons

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,19 @@
+import { type LucideProps } from "lucide-react";
+
+export const DropDown = (props: LucideProps) =>
+  // prettier-ignore
+  <svg width="17" height="17" aria-hidden="true" focusable="false" color="#92929D" {...props} viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M12.793 5.833a.5.5 0 0 1 .39.812L8.89 12.012a.5.5 0 0 1-.78 0L3.817 6.645a.5.5 0 0 1 .39-.812h8.586Z" fill="currentcolor"/>
+  </svg>;
+
+export const MailFast = (props: LucideProps) =>
+  // prettier-ignore
+  <svg width="18" height="18" aria-hidden="true" focusable="false" color="#fff" {...props} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 9V5.25a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v7.5a3 3 0 0 1-3 3H6M4.5 6l2.836 1.89a3 3 0 0 0 3.328 0L13.5 6m-12 5.25H6M1.5 13.5H6" stroke="currentcolor" strokeLinecap="round"/></svg>;
+
+export const CalendarCheck = (props: LucideProps) =>
+  // prettier-ignore
+  <svg width="24" height="24" aria-hidden="true" focusable="false" color="#F44A24" {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 3.75h-1.25V3a.75.75 0 0 0-1.5 0v.75h-6.5V3a.75.75 0 0 0-1.5 0v.75H6c-2.418 0-3.75 1.332-3.75 3.75V18c0 2.418 1.332 3.75 3.75 3.75h12c2.418 0 3.75-1.332 3.75-3.75V7.5c0-2.418-1.332-3.75-3.75-3.75ZM6 5.25h1.25V6a.75.75 0 0 0 1.5 0v-.75h6.5V6a.75.75 0 0 0 1.5 0v-.75H18c1.577 0 2.25.673 2.25 2.25v.75H3.75V7.5c0-1.577.673-2.25 2.25-2.25Zm12 15H6c-1.577 0-2.25-.673-2.25-2.25V9.75h16.5V18c0 1.577-.673 2.25-2.25 2.25Zm-2.97-7.447a.75.75 0 0 1 0 1.061l-3.333 3.333a.748.748 0 0 1-1.06 0L8.97 15.53a.75.75 0 1 1 1.061-1.061l1.137 1.136 2.803-2.803a.749.749 0 0 1 1.059.001Z" fill="currentcolor"/></svg>;
+
+export const CalendarPlus = (props: LucideProps) =>
+  // prettier-ignore
+  <svg width="20" height="20" aria-hidden="true" focusable="false" color="#fff" {...props} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.167 16.042h-5c-1.314 0-1.875-.56-1.875-1.875V7.292h13.75v1.875a.625.625 0 0 0 1.25 0v-3.75c0-2.015-1.11-3.125-3.125-3.125h-1.042v-.625a.625.625 0 0 0-1.25 0v.625H6.458v-.625a.625.625 0 0 0-1.25 0v.625H4.167c-2.015 0-3.125 1.11-3.125 3.125v8.75c0 2.015 1.11 3.125 3.125 3.125h5a.625.625 0 0 0 0-1.25Zm-5-12.5h1.041v.625a.625.625 0 0 0 1.25 0v-.625h5.417v.625a.625.625 0 0 0 1.25 0v-.625h1.042c1.314 0 1.875.56 1.875 1.875v.625H2.292v-.625c0-1.314.56-1.875 1.875-1.875ZM6.683 10c0 .46-.372.834-.833.834A.837.837 0 0 1 5.012 10c0-.46.37-.833.83-.833h.008c.46 0 .833.373.833.833Zm3.334 0c0 .46-.373.834-.834.834A.837.837 0 0 1 8.346 10c0-.46.369-.833.829-.833h.008c.46 0 .834.373.834.833Zm6.65 5c0 .345-.28.625-.625.625h-.417v.417a.625.625 0 0 1-1.25 0v-.417h-.417a.625.625 0 0 1 0-1.25h.417v-.416a.625.625 0 0 1 1.25 0v.416h.417c.345 0 .625.28.625.625ZM15 11.042A3.963 3.963 0 0 0 11.042 15 3.963 3.963 0 0 0 15 18.96 3.963 3.963 0 0 0 18.958 15 3.963 3.963 0 0 0 15 11.042Zm0 6.667A2.711 2.711 0 0 1 12.292 15 2.711 2.711 0 0 1 15 12.292 2.711 2.711 0 0 1 17.708 15 2.711 2.711 0 0 1 15 17.71Zm-8.317-4.375c0 .46-.372.833-.833.833a.837.837 0 0 1-.838-.833c0-.46.37-.834.83-.834h.008c.46 0 .833.374.833.834Z" fill="currentcolor"/></svg>;
---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+import { Clock } from "lucide-react";
+import * as CustomIcons from "./icons";
+
+export const Icons = { Clock, ...CustomIcons };
