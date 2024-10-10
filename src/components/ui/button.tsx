0228607bcd0019b1cb7e7b6d72a57d41405189c6
HusamMohammed <husam.m.ashour@gmail.com>
2024-10-10 23:25:46 +0300

    add custom variant (no styles) to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,6 +22,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-primary/10 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+        custom: "",
       },
       size: {
         default: "h-10 px-4 py-2",
